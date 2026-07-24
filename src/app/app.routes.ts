70605c84113f3518c860949b2df1e4810e2ba61f
Rashminda121 <rashminda121@gmail.com>
2026-07-25 00:57:43 +0530

    feat: add dashboard, transactions, and not-found routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,24 @@
+import { NotFound } from './features/not-found/not-found';
+import { TransactionList } from './features/transactions/transaction-list/transaction-list';
+import { Dashboard } from './features/dashboard/dashboard';
+import { TransactionDetail } from './features/transactions/transaction-detail/transaction-detail';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'transactions',
+    component: TransactionList,
+  },
+  {
+    path: 'transactions/:id',
+    component: TransactionDetail,
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
 ];
