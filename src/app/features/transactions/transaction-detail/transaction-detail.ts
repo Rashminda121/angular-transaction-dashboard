aa5a6757830a506289681fc5e9e3449af94f04c5
Rashminda121 <rashminda121@gmail.com>
2026-07-25 00:02:10 +0530

    feat: add dashboard, transactions, and not-found features
    - Add Dashboard component
    - Add TransactionList component
    - Add TransactionDetail component
    - Add NotFound component

---
+++ src/app/features/transactions/transaction-detail/transaction-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-detail',
+  imports: [],
+  templateUrl: './transaction-detail.html',
+  styleUrl: './transaction-detail.scss',
+})
+export class TransactionDetail {}
