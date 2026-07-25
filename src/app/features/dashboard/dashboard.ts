ccc5369dc746eb6cfba9fb6fb2d50f6c0c252eee
Rashminda121 <rashminda121@gmail.com>
2026-07-25 23:19:27 +0530

    feat: enhance dashboard component with layout and KPI cards

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+import { Icons } from '@shared/icons/icons';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  protected readonly Icons = Icons;
+}
